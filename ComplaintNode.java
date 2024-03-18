8702a0007e8f2d9db7e43f8891be533fe2bc51e9
SRISHTI GUPTA <122162170+srishtiguptaa@users.noreply.github.com>
2024-03-18 14:13:27 +0530

    Update ComplaintNode.java

--- ComplaintNode.java
+++ ComplaintNode.java
@@ -1,87 +1,61 @@
-import java.util.*;
-
-interface Node {
-    void receiveTransaction(String sender, String transaction);
-    void broadcastTransaction(String transaction);
-}
-
-class CompliantNode implements Node {
-    private Set<String> transactions;
-    private List<CompliantNode> followers;
-
-    public CompliantNode() {
-        this.transactions = new HashSet<>();
-        this.followers = new ArrayList<>();
-    }
-
-    public void addFollower(CompliantNode follower) {
-        this.followers.add(follower);
-    }
-
-    @Override
-    public void receiveTransaction(String sender, String transaction) {
-        // Compliant nodes always accept transactions from trusted nodes
-        this.transactions.add(transaction);
-    }
-
-    @Override
-    public void broadcastTransaction(String transaction) {
-        // Compliant nodes broadcast transactions to their followers
-        for (CompliantNode follower : followers) {
-            follower.receiveTransaction(this.toString(), transaction);
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+public class CompliantNode implements Node {    
+    private static final int NUM_OF_TRUST_ROUNDS = 2;    
+    private boolean[] followees;    
+    private Map<Integer, Integer> followeesScore;    
+    private Set<Transaction> pendingTransactions;    
+    private Transaction markerTxn;    
+    private int round;    
+    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {     
+        pendingTransactions = new HashSet<>();        
+        followeesScore = new HashMap<>();   
+    }   
+    public void setFollowees(boolean[] followees) {      
+        this.followees = followees;   
+    }    
+    public void setPendingTransaction(Set<Transaction> pendingTransactions) {       
+        this.pendingTransactions.clear();       
+        this.pendingTransactions.addAll(pendingTransactions);       
+        if (!pendingTransactions.isEmpty()) {           
+            markerTxn = pendingTransactions.iterator().next();       
+        }   
+    }   
+    public Set<Transaction> sendToFollowers() {      
+        Set<Transaction> sendTransactions = new HashSet<>();       
+        if (round > NUM_OF_TRUST_ROUNDS) {           
+            sendTransactions.addAll(pendingTransactions);           
+            pendingTransactions.clear();       
         }
-    }
-
-    @Override
-    public String toString() {
-        // Just returning a unique identifier for the node
-        return super.toString();
-    }
-}
-
-public class ConsensusAlgorithm {
-    public static void main(String[] args) {
-        Map<String, List<String>> trustRelationships = new HashMap<>();
-        trustRelationships.put("A", Arrays.asList("B", "C"));
-        trustRelationships.put("B", Arrays.asList("D", "E"));
-        trustRelationships.put("C", Collections.singletonList("F"));
-        trustRelationships.put("D", Collections.emptyList());
-        trustRelationships.put("E", Collections.emptyList());
-        trustRelationships.put("F", Collections.emptyList());
-
-        Map<String, CompliantNode> nodes = new HashMap<>();
-        for (String nodeName : trustRelationships.keySet()) {
-            nodes.put(nodeName, new CompliantNode());
-        }
-
-        for (Map.Entry<String, List<String>> entry : trustRelationships.entrySet()) {
-            String nodeName = entry.getKey();
-            List<String> followers = entry.getValue();
-            for (String followerName : followers) {
-                nodes.get(nodeName).addFollower(nodes.get(followerName));
-            }
-        }
-
-        // Simulate consensus algorithm with sample transactions
-        List<String[]> transactions = Arrays.asList(new String[]{"A", "transaction1"},
-                                                     new String[]{"B", "transaction2"});
-        simulateConsensus(nodes, transactions);
-
-        // Print transactions received by each node
-        for (Map.Entry<String, CompliantNode> entry : nodes.entrySet()) {
-            String nodeName = entry.getKey();
-            CompliantNode node = entry.getValue();
-            System.out.println("Node " + nodeName + " received transactions: " + node.transactions);
-        }
-    }
-
-    private static void simulateConsensus(Map<String, CompliantNode> nodes, List<String[]> transactions) {
-        for (String[] transaction : transactions) {
-            String sender = transaction[0];
-            String transactionData = transaction[1];
-            for (CompliantNode node : nodes.values()) {
-                node.receiveTransaction(sender, transactionData);
-            }
-        }
-    }
-}
+        else if (markerTxn != null) {   
+            sendTransactions.add(markerTxn);      
+        }      
+        return sendTransactions;   
+    }  
+    public void receiveFromFollowees(Set<Candidate> candidates) {     
+        round++;      
+        Map<Integer, Set<Transaction>> senderToTxMap = new HashMap<>();    
+        for (Candidate c : candidates) {          
+            if (followees[c.sender]) {            
+                senderToTxMap.computeIfAbsent(c.sender, k -> new HashSet<>()).add(c.tx);        
+            }     
+        }      
+        if (round <= NUM_OF_TRUST_ROUNDS) {       
+            for (int i = 0; i < followees.length; i++) {            
+                if (followees[i]) {                
+                    followeesScore.putIfAbsent(i, 0);                 
+                    if (senderToTxMap.containsKey(i) && senderToTxMap.get(i).size() == 1) {                  
+                        followeesScore.put(i, followeesScore.get(i) + 1);              
+                    }             
+                }         
+            }   
+        } else {      
+            for (int i = 0; i < followees.length; i++) {         
+                if (followees[i] && followeesScore.getOrDefault(i, 0) == NUM_OF_TRUST_ROUNDS) {        
+                    pendingTransactions.addAll(senderToTxMap.getOrDefault(i, new HashSet<>()));           
+                }        
+            }    
+        }  
+    }}
